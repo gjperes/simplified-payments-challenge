91efd04bec6dc6c40134fc6aefd65954b93f243c
gjperes <gjperes@ufpr.br>
2024-01-11 12:02:45 -0300

    initial commit

---
+++ src/test/java/gjperes/desafiobackendpp/DesafioBackendPpApplicationTests.java
@@ -0,0 +1,13 @@
+package gjperes.desafiobackendpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendPpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
